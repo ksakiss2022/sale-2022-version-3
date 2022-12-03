4a1808197863a39ce5d67cec459d3a74da7d8f5f
Swetlana <sweta_zaika@mail.ru>
2022-12-03 18:33:02 +0300

    интернет магазин

--- src/main/java/com/skypro/sale2022version3/Sale2022Version3Application.java
+++ src/main/java/com/skypro/sale2022version3/Sale2022Version3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sale2022Version3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sale2022Version3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sale2022Version3Application.class, args);
+    }
 
 }
