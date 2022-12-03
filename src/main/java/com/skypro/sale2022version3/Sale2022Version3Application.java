7e82dd9f6724d5ea9b60c6c8a6c207ab4b67ba51
Swetlana <sweta_zaika@mail.ru>
2022-12-03 18:30:11 +0300

    Initial commit

---
+++ src/main/java/com/skypro/sale2022version3/Sale2022Version3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.sale2022version3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sale2022Version3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sale2022Version3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/sale2022version3/Sale2022Version3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.sale2022version3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sale2022Version3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
